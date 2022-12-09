702d3a7f8cc97f1471a7a668595cf53346dfaa3a
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-09 14:50:29 +0300

    instance method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def user_tests(level)
+    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
+        .where(user_tests: { user_id: self.id }, tests: { level: level })
+  end
 end
