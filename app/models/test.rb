c9c7a82b3ac3727fbbcaf8211371241e56630d86
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-10 22:20:12 +0300

    fix association for test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_many :questions, dependent: :destroy
   has_many :user_tests, dependent: :destroy
