2401ca1ef4025bc71a587b1ca57f66d66e97d521
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-19 16:33:43 +0300

    correction of remarks

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[index create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @test = Test.find(params[:test_id])
     @questions = @test.questions
     render inline: '<li><% @questions.each do |question| %>
                     <p><%= question.body %></p>
@@ -18,7 +18,6 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @test = Test.find(params[:test_id])
     @question = @test.questions.build(question_params)
 
     if @question.save
@@ -35,6 +34,10 @@ class QuestionsController < ApplicationController
   end
 
   private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
   def find_question
     @question = Question.find(params[:id])
