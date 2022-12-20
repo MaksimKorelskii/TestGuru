c1b3ad45f72542c658ff6850e657928974a19f3c
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-20 19:12:47 +0300

    lesson8_view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header
+    if @question.new_record?
+      "Create New #{@test.title} Question"
+    else
+      "Edit #{@question.test.title} Question"
+    end
+  end
 end
