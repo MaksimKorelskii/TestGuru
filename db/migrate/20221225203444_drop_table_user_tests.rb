0d37128389d09d9ed17367d65799ad9278631586
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-26 00:16:41 +0300

    model TestPassage, migration

---
+++ db/migrate/20221225203444_drop_table_user_tests.rb
@@ -0,0 +1,5 @@
+class DropTableUserTests < ActiveRecord::Migration[6.1]
+  def up
+    drop_table :user_tests, if_exists: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_214407) do
+ActiveRecord::Schema.define(version: 2022_12_25_203444) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2022_12_12_214407) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.integer "current_question_id"
+    t.integer "correct_question", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
@@ -47,13 +59,6 @@ ActiveRecord::Schema.define(version: 2022_12_12_214407) do
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
-  create_table "user_tests", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.index ["test_id"], name: "index_user_tests_on_test_id"
-    t.index ["user_id"], name: "index_user_tests_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -63,8 +68,9 @@ ActiveRecord::Schema.define(version: 2022_12_12_214407) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
-  add_foreign_key "user_tests", "tests"
-  add_foreign_key "user_tests", "users"
 end
