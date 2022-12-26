8e69c447c789f60aa88510718a2b853a8968a22d
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-26 22:07:30 +0300

    result view

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,16 +2,16 @@ module TestPassagesHelper
   def test_passage_result(test_passage)
     if test_passage.successful?
       "<h2 class='success'>
-        #{correct_question_percentage(test_passage)}% You passed!
+        Result #{correct_question_percentage(test_passage)}% You passed!
       </h2>".html_safe
     else
       "<h2 class='failure'>
-      #{correct_question_percentage(test_passage)}% You failed.
+        Result #{correct_question_percentage(test_passage)}% You failed.
       </h2>".html_safe
     end
   end
 
   def correct_question_percentage(test_passage)
-    (test_passage.correct_questions.to_f) / test_passage.test.questions.count * 100
+    test_passage.correct_questions.to_f / test_passage.test.questions.count * 100
   end
 end
