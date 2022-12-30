ff4a6745539436ec740c3d6ab0d1f20fbee9b86f
Maksim Korelskii <mkorelskii@gmail.com>
2022-12-30 23:05:06 +0300

    refactoring for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      # session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to tests_path
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  validates :name, :email, presence: true
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
   has_secure_password
 
