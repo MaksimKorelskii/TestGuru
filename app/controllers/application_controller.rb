e0f42df76f3b86b2a6a352d61105aa72721b6cd0
Maksim Korelskii <mkorelskii@gmail.com>
2023-01-14 15:28:02 +0300

    initial localize, applocation_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :hello_flash_message, only: :create, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+    # { lang: I18n.locale }
+  end
 
   private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
