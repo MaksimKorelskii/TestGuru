f04849ec74f6919787d494170d59c664df1243a0
Maksim Korelskii <mkorelskii@gmail.com>
2023-01-20 23:55:00 +0300

    correction of remarks

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -4,11 +4,12 @@ class GistsController < ApplicationController
   def create
     @test_passage = TestPassage.find(params[:id])
     @question = @test_passage.current_question
-    result = GistQuestionService.new(@question).call
+    result = GistQuestionService.new(@question)
+    gist = result.call
 
-    flash_options = if result.html_url.present?
-      current_user.gists.create!(question: @question, url: result.html_url)
-      { notice: t('.success', gist_url: view_context.link_to('Gist', result.html_url)) }
+    flash_options = if result.success?
+      current_user.gists.create!(question: @question, url: gist.html_url)
+      { notice: t('.success', gist_url: view_context.link_to('Gist', gist.html_url)) }
     else
       { alert: t('.failure') }
     end
--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -9,6 +9,10 @@ class GistQuestionService
     @client.create_gist(gist_params)
   end
 
+  def success?
+    @client.last_response.status == 201
+  end
+
   private
 
   def gist_params
